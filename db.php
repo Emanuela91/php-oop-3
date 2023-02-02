c401c98affc42969afe42f5c5c7ce5ebd70a96ef
Emanuela91 <emanuela.marson91@gmail.com>
2023-02-02 17:45:57 +0100

    aggiornato tutto su una pagina

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-include __DIR__ . '/models/salary.php';
-include __DIR__ . '/models/person.php';
-include __DIR__ . '/models/employee.php';
-include __DIR__ . '/models/chief.php';
-
-$stipendio1 = new Stipendio(1800, true, true);
-
-?>
